e25e15a3d8df1dac7aa0440fef8f1c7dd33d744d
oshark89 <omarelsharkawy67@gmail.com>
2024-02-28 07:05:44 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,12 +8,12 @@ const port = 8000;
 
 /**create an object from express */
 const app = express();
-const allowedOrigin = process.env.ALLOWED_ORIGIN || 'https://car-renting-project-2th5.vercel.app'; // Replace with your actual frontend URL
 
 /**tell what our app to use */
 
+app.use(cors());
 app.use(cors({
-    origin: allowedOrigin // Use the allowed origin variable
+    origin: 'https://car-renting-project.vercel.app/'
   }));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
